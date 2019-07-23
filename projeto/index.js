027ebd350ace3437a577687fccf17e8f9d5ddfb5
PauloNayron <paulonayron.s@hotmail.com>
2019-07-22 21:53:21 -0300

    feat(projeto): retornando Arrays

--- projeto/index.js
+++ projeto/index.js
@@ -25,6 +25,7 @@ const typeDefs = gql`
         horaAtual: Date!
         usuarioLogado: Usuario
         produtoEmDestaque: Produto
+        numerosMegaSena: [Int]
     }
 `
 
@@ -64,6 +65,12 @@ const resolvers = {
                 preco: 4890.89,
                 desconto: 0.15
             }
+        },
+        numerosMegaSena() {
+            const crescente = (a, b) => a - b
+            return Array(6).fill(0)
+                .map(n => parseInt(Math.random() * 60 + 1))
+                .sort(crescente)
         }
     }
 }
